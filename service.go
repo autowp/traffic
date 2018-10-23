ab4c6d59370faa91f15beeecde3839a6d9a31fa9
Dmitry P <dmitry@pereslegin.ru>
2018-10-24 00:47:20 +0300

    Wait for rabbitmq

--- service.go
+++ service.go
@@ -78,6 +78,8 @@ var AutobanProfiles = []AutobanProfile{
 // NewService constructor
 func NewService(config Config) (*Service, error) {
 
+	var err error
+
 	logger := util.NewLogger(config.Rollbar)
 
 	loc, _ := time.LoadLocation("UTC")
@@ -89,7 +91,7 @@ func NewService(config Config) (*Service, error) {
 
 	var db *sql.DB
 	for {
-		db, err := sql.Open("mysql", config.DSN)
+		db, err = sql.Open("mysql", config.DSN)
 		if err != nil {
 			return nil, err
 		}
@@ -108,12 +110,28 @@ func NewService(config Config) (*Service, error) {
 		time.Sleep(100 * time.Millisecond)
 	}
 
-	wg := &sync.WaitGroup{}
+	start = time.Now()
+	timeout = 60 * time.Second
 
-	rabbitMQ, err := amqp.Dial(config.RabbitMQ)
-	if err != nil {
-		return nil, err
+	fmt.Println("Waiting for rabbitMQ")
+
+	var rabbitMQ *amqp.Connection
+	for {
+		rabbitMQ, err = amqp.Dial(config.RabbitMQ)
+		if err == nil {
+			break
+		}
+
+		if time.Since(start) > timeout {
+			logger.Fatal(err)
+			return nil, err
+		}
+
+		fmt.Print(".")
+		time.Sleep(100 * time.Millisecond)
 	}
+
+	wg := &sync.WaitGroup{}
 
 	whitelist, err := NewWhitelist(db, loc)
 	if err != nil {
@@ -235,14 +253,18 @@ func (s *Service) Close() {
 
 	s.waitGroup.Wait()
 
-	err := s.db.Close()
-	if err != nil {
-		s.logger.Warning(err)
+	if s.db != nil {
+		err := s.db.Close()
+		if err != nil {
+			s.logger.Warning(err)
+		}
 	}
 
-	err = s.rabbitMQ.Close()
-	if err != nil {
-		s.logger.Warning(err)
+	if s.rabbitMQ != nil {
+		err := s.rabbitMQ.Close()
+		if err != nil {
+			s.logger.Warning(err)
+		}
 	}
 }
 
