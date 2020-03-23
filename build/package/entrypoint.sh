4d486c1d6efb97175303961e12d573e92a360454
Dmitry P <dmitry@pereslegin.ru>
2020-03-24 00:38:20 +0300

    Fix build

--- build/package/entrypoint.sh
+++ build/package/entrypoint.sh
@@ -1,5 +1,3 @@
-mkdir -p /var/log/supervisor
-
 waitforit -address tcp://$TRAFFIC_RABBITMQ_HOST:$TRAFFIC_RABBITMQ_PORT -timeout 30
 
-/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+./traffic
